819285d61c64ab48d890088ee0d80c4e22f6854f
kaique oliveira santos <kaiquevicio@gmail.com>
2021-09-10 14:10:52 -0300

    Pagination

---
+++ backend/src/main/java/com/dsvendas/services/SaleService.java
@@ -0,0 +1,28 @@
+package com.dsvendas.services;
+
+import com.dsvendas.dto.SaleDTO;
+import com.dsvendas.entity.Sale;
+import com.dsvendas.repository.SaleRepository;
+import com.dsvendas.repository.SellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class SaleService {
+
+    @Autowired
+    private SaleRepository saleRepository;
+
+    @Autowired
+    private SellerRepository sellerRepository;
+
+    @Transactional(readOnly = true)
+    public Page<SaleDTO> findAll(Pageable pageable) {
+        sellerRepository.findAll();
+        Page<Sale> result = saleRepository.findAll(pageable);
+        return result.map(x -> new SaleDTO(x));
+    }
+}
